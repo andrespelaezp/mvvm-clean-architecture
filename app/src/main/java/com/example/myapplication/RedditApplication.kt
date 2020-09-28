6009a49b9126266d4066a9d18805fe4e24ca4973
Andres Pelaez <andrespelaez24@gmail.com>
2020-09-27 20:16:32 -0500

    Base architecture setup

---
+++ app/src/main/java/com/example/myapplication/RedditApplication.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import com.example.myapplication.di.ApiModule
+import com.example.myapplication.di.ApplicationModule
+import com.example.myapplication.di.DaggerApplicationComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+class RedditApplication : DaggerApplication() {
+
+    init {
+        INSTANCE = this
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerApplicationComponent
+            .builder()
+            .apiModule(ApiModule(getString(R.string.base_url)))
+            .applicationModule(ApplicationModule(this))
+            .create(this)
+    }
+
+    companion object {
+        @JvmStatic
+        lateinit var INSTANCE: RedditApplication
+    }
+}
